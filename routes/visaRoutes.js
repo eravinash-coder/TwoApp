84cc83280dc5b8016b3776fd1289066f1e5b6a87
unknown <akt7273922921@gmail.com>
2024-01-27 19:23:32 +0530

    first commit

--- routes/visaRoutes.js
+++ routes/visaRoutes.js
@@ -3,19 +3,26 @@ const router = express.Router();
 const path = require('path');
 const bodyParser = require('body-parser');
 const visaController = require('../controllers/visaController');
-
-
-router.use(bodyParser.urlencoded({ extended: true }));
-router.use(express.static(path.resolve(__dirname, 'public')));
-
 const multer = require('multer');
+const user = express();
 
-const uploader = multer({
-  storage: multer.memoryStorage(),
-  limits: { fileSize: 500000 }
+
+
+user.use(bodyParser.urlencoded({ extended:true }));
+user.use(express.static(path.resolve(__dirname,'public')));
+
+var storage = multer.diskStorage({
+    destination:(req,file,cb) => {
+        cb(null,'./public/uploads')
+    },
+    filename:(req,file,cb) =>{
+        cb(null,file.originalname)
+    }
 });
 
-router.post('/addvisa', uploader.single("file"), visaController.addVisa);
+var upload = multer({ storage:storage });
+
+router.post('/addvisa', upload.single("file"), visaController.addVisa);
 
 
 module.exports = router;
