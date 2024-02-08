cc956ed37e3e57ae25bbfaf366384a041d8fb44b
unknown <akt7273922921@gmail.com>
2024-02-08 23:46:25 +0530

    first commit

--- server.js
+++ server.js
@@ -33,11 +33,6 @@ connectDB();
 
 const app = express();
 
-
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-app.use(express.json());
-
 app.use(express.json())
 
 if (process.env.NODE_ENV === 'development') {
