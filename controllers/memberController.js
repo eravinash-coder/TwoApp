5123fc4abd3530661057517cab19b3816a633398
unknown <akt7273922921@gmail.com>
2024-02-17 12:58:34 +0530

    first commit

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -3,6 +3,8 @@ const Association = require('../models/Association');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const asyncHandler = require("express-async-handler");
+const excel = require('exceljs');
+const fs = require('fs');
 
 exports.register = async (req, res) => {
   try {
@@ -117,4 +119,51 @@ exports.deleteMember = asyncHandler(async (req, res) => {
     msg: 'Successfully Deleted',
     data: member
   });
+});
+exports.addMemberBulk= asyncHandler(async (req, res) => {
+  const associationId = req.user.associationId;
+  try {
+    
+    const filePath = req.file.path;
+
+    // Read data from Excel file
+    const workbook = new excel.Workbook();
+    await workbook.xlsx.readFile(filePath);
+    const worksheet = workbook.getWorksheet(1); // Assuming data is on the first worksheet
+    const data = [];
+
+    for (let i = 2; i <= worksheet.rowCount; i++) {
+      const row = worksheet.getRow(i);
+
+      const email = row.getCell(2).value;
+      const existingMember = await Member.findOne({ email });
+
+      if (existingMember) {
+        console.log(`Skipping data with email ${email} as it already exists`);
+        continue; // Skip insertion if email already exists
+      }
+
+      const name = row.getCell(1).value;
+      const password = row.getCell(3).value;
+
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Create new member instance
+      const newMember = new Member({
+        associationId,
+        name,
+        email,
+        password: hashedPassword // Store the hashed password
+      });
+
+      await newMember.save();
+      console.log(`Data inserted successfully for email: ${email}`);
+    }
+    fs.unlinkSync(filePath);
+    res.send('Data import complete');
+  } catch (error) {
+    console.error('Error importing data:', error);
+    res.status(500).send('Error importing data');
+  }
 });
--- routes/memberRoutes.js
+++ routes/memberRoutes.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const memberController = require('../controllers/memberController');
 const authassoMiddleware = require('../middleware/authAssoMiddleware');
+const multer = require('multer'); // Import multer for file upload handling
+const upload = multer({ dest: '../uploads/' });
 
-router.post('/register',authassoMiddleware, memberController.register);
+router.post('/register', authassoMiddleware, memberController.register);
+router.post('/addMemberBulk', upload.single('excelFile'), authassoMiddleware, memberController.addMemberBulk);
 router.post('/login', memberController.login);
 router.get('/getMember',authassoMiddleware, memberController.getMember);
 router.get('/getById/:memberId',memberController.getMemberById);
