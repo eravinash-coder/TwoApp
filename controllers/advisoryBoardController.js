b253b3c74aad605e50c77c4191a6d9ecee722861
unknown <akt7273922921@gmail.com>
2024-03-07 18:01:17 +0530

    first commit

--- controllers/advisoryBoardController.js
+++ controllers/advisoryBoardController.js
@@ -75,4 +75,90 @@ exports.getAdvisoryBoard  = async (req, res) => {
     } catch (error) {
       res.status(500).send(error.message);
     }
-  };
+};
+
+
+exports.getAdvisoryBoardsById = asyncHandler(async (req, res) => {
+    const AdvisoryBoards = await AdvisoryBoard.findById(req.params.AdvisoryBoardId)
+
+
+
+
+    res.json({
+        success: true,
+        data: AdvisoryBoards,
+    });
+
+});
+
+
+exports.getAllAdvisoryBoards = asyncHandler(async (req, res) => {
+
+    
+   
+    let AdvisoryBoards = await AdvisoryBoard.find();
+
+
+
+    res.json({
+        success: true,
+        data: AdvisoryBoards,
+    });
+});
+
+exports.editAdvisoryBoards = asyncHandler(async (req, res) => {
+    try {
+        let advisoryBoard = await AdvisoryBoard.findById(req.params.AdvisoryBoardId);
+
+        if (!advisoryBoard) {
+            return res.status(401).json({
+                success: false,
+                msg: 'AdvisoryBoards not found.'
+            })
+        }
+        await runMiddleware(req, res, myUploadMiddleware);
+        const { name,post, about,  } = req.body;
+        let imageObjects;
+
+        if (req.files && req.files['image']) {
+            imageObjects = await Promise.all(
+                req.files['image'].map(async (file) => {
+                    const b64 = Buffer.from(file.buffer).toString('base64');
+                    const dataURI = 'data:' + file.mimetype + ';base64,' + b64;
+                    return handleUpload(dataURI);
+                })
+            );
+        }
+
+        advisoryBoard = await AdvisoryBoard.findByIdAndUpdate(req.params.AdvisoryBoardId, { name, post, about,  images: imageObjects }, {
+            new: true,
+            runValidators: true
+        });
+
+        res.status(200).json({ success: true, data: advisoryBoard, msg: 'Successfully updated' });
+
+
+    } catch (error) {
+        // Send error response inside the catch block
+        res.status(400).json({ success: false, msg: error.message });
+    }
+});
+
+
+exports.deleteAdvisoryBoards = asyncHandler(async (req, res) => {
+
+    const advisoryBoard = await AdvisoryBoard.findByIdAndDelete(req.params.AdvisoryBoardId);
+
+    if (!advisoryBoard) {
+        return res.status(401).json({
+            success: false,
+            msg: 'Member not found.'
+        });
+    }
+
+    res.status(201).json({
+        success: true,
+        msg: 'Successfully Deleted',
+        data: advisoryBoard
+    });
+});
--- routes/advisoryBoardRoutes.js
+++ routes/advisoryBoardRoutes.js
@@ -6,5 +6,9 @@ const advisoryBoardController = require('../controllers/advisoryBoardController'
 
 router.post('/addAdvisoryBoard', advisoryBoardController.addAdvisoryBoard);
 router.get('/getAdvisoryBoard', advisoryBoardController.getAdvisoryBoard);
+router.get('/getAllAdvisoryBoards', advisoryBoardController.getAllAdvisoryBoards);
+router.get('/getById/:AdvisoryBoardId',advisoryBoardController.getAdvisoryBoardsById);
+router.put('/editAdvisoryBoard/:AdvisoryBoardId',advisoryBoardController.editAdvisoryBoards);
+router.delete('/deleteAdvisoryBoard/:AdvisoryBoardId',advisoryBoardController.deleteAdvisoryBoards)
 
 module.exports = router;
